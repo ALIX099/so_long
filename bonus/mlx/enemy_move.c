940d807ae86115a28e5151d1e8387bcba788c164
Ali Bouknana <abouknan@e2r11p5.1337.ma>
2025-04-08 15:15:30 +0100

    Tue Apr  8 03:15:30 PM +01 2025

--- bonus/mlx/enemy_move.c
+++ bonus/mlx/enemy_move.c
@@ -6,7 +6,7 @@
 /*   By: abouknan <abouknan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 09:57:25 by abouknan          #+#    #+#             */
-/*   Updated: 2025/04/07 22:40:46 by abouknan         ###   ########.fr       */
+/*   Updated: 2025/04/08 15:15:13 by abouknan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,28 +22,28 @@ static void	put_enemy_image(t_game *game, int x, int y, int new_x)
 {
 	int	new_y;
 
-	if (game->map[y][x] == 'T')
+	game->map[y][x] = '0';
+	mlx_put_image_to_window(game->mlx_init, game->mlx_win, game->back, x * 40, y
+		* 40);
+	new_x = x + (custom_rand(&game->enemy_seed) - 1);
+	new_y = y + (custom_rand(&game->enemy_seed) - 1);
+	if (new_x >= 0 && new_x < game->map_x && game->map[y][new_x] == '0')
 	{
-		game->map[y][x] = '0';
-		mlx_put_image_to_window(game->mlx_init, game->mlx_win, game->back, x
-			* 40, y * 40);
-		new_x = x + (custom_rand(&game->enemy_seed) - 1);
-		new_y = y + (custom_rand(&game->enemy_seed) - 1);
-		if (new_x >= 0 && new_x < game->map_x && game->map[y][new_x] == '0')
-		{
-			game->map[y][new_x] = 'T';
-			mlx_put_image_to_window(game->mlx_init, game->mlx_win,
-				game->img_killer[game->killer_frame], new_x * 40, y * 40);
-		}
-		else if (new_y >= 0 && new_y < game->map_y
-			&& game->map[new_y][x] == '0')
-		{
-			game->map[new_y][x] = 'T';
-			mlx_put_image_to_window(game->mlx_init, game->mlx_win,
-				game->img_killer[game->killer_frame], x * 40, new_y * 40);
-		}
-		else
-			game->map[y][x] = 'T';
+		game->map[y][new_x] = 'T';
+		mlx_put_image_to_window(game->mlx_init, game->mlx_win,
+			game->img_killer[game->killer_frame], new_x * 40, y * 40);
+	}
+	else if (new_y >= 0 && new_y < game->map_y && game->map[new_y][x] == '0')
+	{
+		game->map[new_y][x] = 'T';
+		mlx_put_image_to_window(game->mlx_init, game->mlx_win,
+			game->img_killer[game->killer_frame], x * 40, new_y * 40);
+	}
+	else
+	{
+		game->map[y][x] = 'T';
+		mlx_put_image_to_window(game->mlx_init, game->mlx_win,
+			game->img_killer[game->killer_frame], x * 40, y * 40);
 	}
 }
 
@@ -60,7 +60,8 @@ void	move_enemies(t_game *game)
 		x = 0;
 		while (game->map[y][x])
 		{
-			put_enemy_image(game, x, y, new_x);
+			if (game->map[y][x] == 'T')
+				put_enemy_image(game, x, y, new_x);
 			x++;
 		}
 		y++;
